0a55591211d10b5c204fb8c6195489a447c965a5
Yuşa Çetin <yusha493@gmail.com>
2022-05-06 16:06:25 +0300

    update 1.1

--- README.md
+++ README.md
@@ -1,14 +1,22 @@
-### (Very) Small but impactful enhancements to Google Earth Web
-Do you hate how you have to click the street view button at the bottom right of the screen again every time you click the button all the way at the top left of the screen to quit street view? Do you hate how it doesn't reset to top-north view afterwards so you also have to click the compass button? Then you might like this Chrome extension.
+### (Very) Small but impactful enhancements to Google Earth Web for Chrome
+Do you love Google Earth but hate how you have to click the street view button at the bottom right of the screen again every time you click the button all the way at the top left of the screen to quit street view? Do you hate how it doesn't reset to top-north view afterwards so you also have to click the compass button? Do you hate how you have to move your hand all over the keyboard to use some of the shortcuts? Then you might like this Chrome extension.
 
 ### Installation
-Make sure developer mode is enabled in Chrome, then go to [chrome://extensions](chrome://extensions), click "Load unpacked" and select the folder containing the files.
+* Clone or download the files to a directory.
+* Make sure developer mode is enabled in Chrome
+* Go to [chrome://extensions](chrome://extensions)
+* Click "Load unpacked" at the top left
+* Select the directory containing the files
+Note: You may need to press Z every time you open Google Earth in a new tab to make some of the shortcuts work.
 
 ### What does it do?
-The extension maps the F key to the top-north view shortcut and the D key to the street view toggle button. You can also press D while in street view mode to quit it and go back to map view mode.
+The main reason I wanted to create this was to make entering and exiting street view mouse-click-free. The extension maps the F key to the top-north view shortcut and the D and E keys to the street view toggle button by default. You can also press D or E while in street view mode to quit it and go back to map view mode. It also modifies many other shortcuts that are scattered all over the keyboard to bring them near the left hand home row so you can use them without lifting your hand.
 
 ### Why?
-You can keep your left hand at its natural home row position on the keyboard and save yourself 3 button clicks (that are positioned at the opposite corners of the screen) every time you enter and exit street view.
+You can keep your left hand at its natural home row position on the keyboard and save yourself many mouse clicks to make your Google Earth experience much smoother.
+
+### How do I customize the keys?
+Edit the constants at the top of the content_script.js file.
 
 ### More features?
 Don't count on it. Feel free to fork. 
@@ -17,4 +25,5 @@ Don't count on it. Feel free to fork.
 Zlib. See [https://opensource.org/licenses/Zlib](https://opensource.org/licenses/Zlib).
 
 ### Known issues
-Sometimes F doesn't work. Reloading fixes. Not sure if the problem is with the extension or Google Earth as the compass button tends to break sometimes.
+* Most of the time some shortcuts don't work at first. Simply press Z once the page loads to fix.
+* If your computer is slow, disabling certain shortcuts when search is on might lag and remove focus from search. Wait for a short while after opening search or first press something that isn't mapped to a shortcut as a workaround. If your computer is slow enough to be impacted by this you are probably used to stuff like this.
---
+++ content_script.js
@@ -0,0 +1,110 @@
+// shortcuts that simulate clicks
+const svkeys = ["d", "e"]; // shortcuts for street view
+const randomkeys = ["a"]; // shortcuts for random location aka I'm Feeling Lucky
+const voyagerkeys = ["g"]; // shortcut for voyager (mapping this to "v" by default might be more intuitive but i never use this feature so i want measure to be more accessible (see measurekey below))
+
+// shortcuts that modify existing shortcuts
+// i don't know if these can be assigned to multiple keys with this method
+const northkey = "f"; // shortcut for top-north view
+const measurekey = "v"; // shortcut for distance measurement
+const searchkey = "s"; // shortcut for search
+const mapstylekey = "w"; // shortcut for map style
+const projectskey = "p"; // shortcut for projects (i never use it so i don't want it to occupy valuable space)
+const settingskey = "t"; // shortcut for settings
+
+// if the modified shortcuts don't work automatically, press this once
+// when the page loads to register them again
+const fixkey = "z";
+
+document.onkeydown = function(e){
+    if (window.location.href.indexOf("search") >= 0){
+        return;
+    }
+
+    if (svkeys.indexOf(e.key.toLowerCase()) >= 0){
+        const app = document.getElementsByTagName("earth-app")[0].shadowRoot;
+        const sv = app.getElementById("street-view").shadowRoot;
+        const svbutton = sv.getElementById("pegman-icon");
+        svbutton.click();
+        return;
+    }
+
+    else if (randomkeys.indexOf(e.key.toLowerCase()) >= 0){
+        const app = document.getElementsByTagName("earth-app")[0].shadowRoot;
+        const toolbar = app.getElementById("toolbar").shadowRoot;
+        const randombutton = toolbar.getElementById("random-icon");
+        randombutton.click();
+        return;
+    }
+
+    else if (voyagerkeys.indexOf(e.key.toLowerCase()) >= 0){
+        const app = document.getElementsByTagName("earth-app")[0].shadowRoot;
+        const toolbar = app.getElementById("toolbar").shadowRoot;
+        const voyagerbutton = toolbar.getElementById("feed");
+        voyagerbutton.click();
+        return;
+    }
+
+    else if(e.key.toLowerCase() == fixkey){
+        registerModifiedShortcuts();
+    }
+}
+
+function registerModifiedShortcuts(){
+    registerNorthKey();
+    registerToolbarShortcuts();
+    registerMenuShortcuts();
+}
+
+function registerNorthKey(){
+    const app = document.getElementsByTagName("earth-app")[0].shadowRoot;
+    const compass = app.getElementById("compass").shadowRoot;
+    // getElementsByTagName doesn't work so we iterate over the children to check
+    for (let i=0; i<compass.childNodes.length; i++){
+        if (compass.childNodes[i].tagName.toLowerCase() == "earth-kb-shortcut"){
+            if (compass.childNodes[i].getAttribute("keys").toLowerCase() == "n"){
+                compass.childNodes[i].setAttribute("keys", northkey);
+                break;
+            }
+        }
+    }
+}
+
+function registerToolbarShortcuts(){
+    const app = document.getElementsByTagName("earth-app")[0].shadowRoot;
+    const toolbar = app.getElementById("toolbar").shadowRoot;
+    for (let i=0; i<toolbar.childNodes.length; i++){
+        if (toolbar.childNodes[i].tagName.toLowerCase() == "earth-kb-shortcut"){
+            switch (toolbar.childNodes[i].getAttribute("keys").toLowerCase()){
+                case "/":
+                    toolbar.childNodes[i].setAttribute("keys", searchkey);
+                    break;
+                case "p":
+                    toolbar.childNodes[i].setAttribute("keys", projectskey);
+                    break;
+                case "m":
+                    toolbar.childNodes[i].setAttribute("keys", mapstylekey);
+                    break;
+                case "shift+m":
+                    toolbar.childNodes[i].setAttribute("keys", measurekey);
+                    break;
+            }
+        }
+    }
+}
+
+function registerMenuShortcuts(){
+    const app = document.getElementsByTagName("earth-app")[0].shadowRoot;
+    const menu = app.getElementById("menu").shadowRoot;
+    for (let i=0; i<menu.childNodes.length; i++){
+        if (menu.childNodes[i].tagName.toLowerCase() == "earth-kb-shortcut"){
+            switch (menu.childNodes[i].getAttribute("keys").toLowerCase()){
+                case ",":
+                    menu.childNodes[i].setAttribute("keys", settingskey);
+                    break;
+            }
+        }
+    }
+}
+
+registerModifiedShortcuts();
